a30be47ab00236129fd7beef24e7195de0107f3f
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-10 17:28:48 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+
+        for(char c : s.toCharArray()) {
+            charMap.put(c, charMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()) {
+            charMap.put(c, charMap.getOrDefault(c, 0) - 1);
+
+            if(charMap.get(c) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
