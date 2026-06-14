e0f6b4c12d7359c7b49bfbaa9e65b3d058337dbf
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-13 20:14:44 -0500

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> hashMap;
+
+        for(int i = 0; i < nums.size(); ++i) {
+            auto it = hashMap.find(target - nums[i]);
+            if(it != hashMap.end()) {
+                return {
+                    std::min(it->second, i),
+                    std::max(it->second, i)
+                };
+            }
+            hashMap[nums[i]] = i;
+        }
+        return {0,0};
+    }
+};
