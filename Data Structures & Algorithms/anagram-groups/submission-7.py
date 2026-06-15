d4b703f4f33c54055e358603d1510018549a4520
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-14 19:10:19 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = dict()
+
+        for str in strs:
+            sortedStr = "".join(sorted(str))
+            foundList = map.get(sortedStr, None)
+
+            if(foundList is not None):
+                foundList.append(str)
+            else:
+                map[sortedStr] = [str]
+        output = []
+        for strList in map.values():
+            output.append(strList)
+        return output
+
+
+
+
+        
