2802612f113b3663e6e27beedb311f897e203c0a
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-15 11:16:43 -0500

    Add: design-hashset - submission-6

---
+++ Data Structures & Algorithms/design-hashset/submission-6.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+
+    public MyHashSet() {
+        this._hashSet = new boolean[10000001];;
+    }
+    
+    public void add(int key) {
+        this._hashSet[key] = true;
+    }
+    
+    public void remove(int key) {
+        this._hashSet[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return this._hashSet[key];
+    }
+
+    boolean[] _hashSet;
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
