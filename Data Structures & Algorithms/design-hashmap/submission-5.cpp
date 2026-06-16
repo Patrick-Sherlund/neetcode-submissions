0df853c957efea7cfd8772f93b87a85631a6dfba
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-16 12:57:20 -0500

    Add: design-hashmap - submission-5

---
+++ Data Structures & Algorithms/design-hashmap/submission-5.cpp
@@ -0,0 +1,49 @@
+class MyHashMap {
+public:
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        std::vector<int>* vec = find(key);
+        if(vec != nullptr) {
+            (*vec)[1] = value;
+        } else {
+            m_hashMap.push_back({key, value});
+        }
+    }
+    
+    int get(int key) {
+        std::vector<int>* vec = find(key);
+        if(vec != nullptr) {
+            return (*vec)[1];
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        std::vector<int>* vec = find(key);
+        if(vec != nullptr) {
+            (*vec)[1] = -1;
+            (*vec)[0] = -1;
+        }    
+    }
+private:
+    std::vector<int>* find(int key) {
+        for(std::vector<int>& vec : m_hashMap) {
+            if(vec[0] == key) {
+                return &vec;
+            }
+        }
+        return nullptr;
+    }
+    std::vector<std::vector<int>> m_hashMap;
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
