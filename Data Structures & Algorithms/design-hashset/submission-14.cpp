c320d3bbe9137e4bceb5305b9e7b9bfabeaa12b7
Patrick Sherlund <patricksherlund@Gmail.com>
2026-06-16 23:29:37 -0500

    Add: design-hashset - submission-14

---
+++ Data Structures & Algorithms/design-hashset/submission-14.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+
+class MyHashSet {
+public:
+    MyHashSet() : m_hashSet(1000001, false) {}
+    
+    void add(int key) {
+        m_hashSet[key] = true;
+    }
+    
+    void remove(int key) {
+        m_hashSet[key] = false;
+    }
+    
+    bool contains(int key) {
+        return m_hashSet[key];
+    }
+private:
+    std::vector<int> m_hashSet;
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
